797418e446ac8f48b26450b6d00c4acec46132e6
Annny-tech <anujmeshram999@gmail.com>
2026-06-09 13:56:59 +0530

    add loops

--- Terraform-Modules-EC2/main.tf
+++ Terraform-Modules-EC2/main.tf
@@ -3,5 +3,5 @@ module "ec2" {
 
   ami_id         = "ami-xxxxxxxxxxxxx"
   instance_type  = "t2.micro"
-  instance_name  = "terraform-module-ec2"
+  instance_name  = "terraform-module-ec2"  
 }
---
+++ Terraform-count-loop/variable.tf
@@ -0,0 +1,11 @@
+variable "instance_count" {
+  default = 3
+}
+
+variable "ami_id" {
+  default = "ami-091138d0f0d41ff90"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
